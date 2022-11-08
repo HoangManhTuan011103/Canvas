35e30fb92029a4189cc86e5c03f96245394e4b07
tuanhm03 <tuanthanhcong2027@gmail.com>
2022-11-08 15:14:26 +0700

    update_lai

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ import './style.css'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-    <img class="anhmoine" src="https://www.facebook.com/profile.php?id=100073742532383" alt="">
+    <a href="https://www.facebook.com/profile.php?id=100073742532383" target="_blank">
+    <img class="anhmoine" src="https://scontent.fhan17-1.fna.fbcdn.net/v/t39.30808-6/305320167_190375380097212_2643486384849884699_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=whun4OQsDI4AX-ZAVUZ&_nc_ht=scontent.fhan17-1.fna&oh=00_AfCYL9I793HicIvcL_i6XsEsQLFc-yrdwp5ZNX0UqN0w3Q&oe=636F7C2C" alt="">
     </a>
     <a class="tramy" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       Trên đây là ảnh con ngu nào đó!
