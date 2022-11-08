d68230db551bf8bb52681d39f65fd4e3745c9023
tuanhm03 <tuanthanhcong2027@gmail.com>
2022-11-08 14:21:21 +0700

    hhihihihihi

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ document.querySelector('#app').innerHTML = `
     <a href="https://vitejs.dev" target="_blank">
     <img class="anhmoine" src="https://scontent.fhan17-1.fna.fbcdn.net/v/t39.30808-6/305320167_190375380097212_2643486384849884699_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=whun4OQsDI4AX-ZAVUZ&_nc_ht=scontent.fhan17-1.fna&oh=00_AfCYL9I793HicIvcL_i6XsEsQLFc-yrdwp5ZNX0UqN0w3Q&oe=636F7C2C" alt="">
     </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      Chào My nhé
+    <a class="tramy" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      Trên đây là ảnh con ngu nào đó!
     </a>
-    <h1>Hello My!</h1>
+    <h1>Hi My! Chào cậu</h1>
     <div class="card">
-    <a href="grid.html">Nhấn vào đây có quà nè</a>
+    <a href="grid.html" class="btn-click--see" >Nhấn vào đây có quà nè</a>
     </div>
     <p class="read-the-docs">
       
