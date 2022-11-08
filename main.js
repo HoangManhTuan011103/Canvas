65682167286a4303d54580495b41fff54a88de3a
tuanhm03 <tuanthanhcong2027@gmail.com>
2022-11-08 22:05:51 +0700

    profile

--- main.js
+++ main.js
@@ -4,16 +4,12 @@ import './style.css'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://www.facebook.com/profile.php?id=100073742532383" target="_blank">
-    <img class="anhmoine" src="https://scontent.fhan17-1.fna.fbcdn.net/v/t39.30808-6/305320167_190375380097212_2643486384849884699_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=whun4OQsDI4AX-ZAVUZ&_nc_ht=scontent.fhan17-1.fna&oh=00_AfCYL9I793HicIvcL_i6XsEsQLFc-yrdwp5ZNX0UqN0w3Q&oe=636F7C2C" alt="">
+    <a href="https://www.facebook.com/profile.php?id=100014186206636" target="_blank">
+    <img class="anhmoine" src="https://scontent.fhan17-1.fna.fbcdn.net/v/t39.30808-6/271589417_1287508111732050_1542460119536826329_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=bI_Coo5FGlsAX-usXZC&_nc_oc=AQkm81blIX0ten297ozb6Jxf7U39hzcRvUD-1KTwzZNn4pIy8jJeFpbRykkwkKV4FNA&_nc_ht=scontent.fhan17-1.fna&oh=00_AfDSlnX2ASl1kQzJPKzHH4EUp4C11n2pnOaVnAfCzJgGiA&oe=636F925F" alt="">
     </a>
-    <a class="tramy" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      Trên đây là ảnh con ngu nào đó!
-    </a>
-    <a class="inforHLU" href="https://www.facebook.com/profile.php?id=100073742532383" > Nhấn Vào Đây Để Đến Facebook Master Chef HLU Cộng Hòa</a>
-    <h1>Hi My! Chào cậu nhé</h1>
+    <h1>Hello! Chào cậu nhé</h1>
     <div class="card">
-    <a href="grid.html" class="btn-click--see" >Nhấn vào đây có quà nè</a>
+    <a href="grid.html" class="btn-click--see" >Nhấn vào đây có bất ngờ nè</a>
     </div>
     <p class="read-the-docs">
       
